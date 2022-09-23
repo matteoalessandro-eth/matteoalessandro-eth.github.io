85e3e3856b8052b18c09c197f68138667c85f034
matteoalessandro-eth <114187992+matteoalessandro-eth@users.noreply.github.com>
2022-09-23 01:25:18 +0000

    Update app.js

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ var input = document.querySelector("input"),
 	block = document.getElementsByTagName("section");
 
 window.onload = function() {
-	typeWriter("#home","true",10);
+	typeWriter("#home","true",1000);
 
 	var sectionArray = [];
 	for(var i = 0;i < block.length;i++) {
